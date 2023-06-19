bd14b4f0a398949cde55e852ea09f2e88f3eebe9
JayPankajPatel <jayp102501@gmail.com>
2023-06-18 23:21:01 -0700

    fixed clipboard

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -3,16 +3,18 @@
 vim.api.nvim_command('set relativenumber')
 
 -- for WSL, it lets you copy and paste and yank to sys clipboard
-vim.g.clipboard = {
-   name = 'WslClipboard',
-   copy = {
-      ['+'] = 'clip.exe',
-      ['*'] = 'clip.exe',
-   },
-   paste = {
-      ['+'] = 'vim.fn.system("powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace(\"`r\", \"\"))")',
-      ['*'] = 'vim.fn.system("powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace(\"`r\", \"\"))")',
-   },
-   cache_enabled = 0,
-}
+if vim.fn.has('wsl') == 1 then
+    vim.g.clipboard = {
+        name = 'WslClipboard',
+        copy = {
+            ['+'] = 'clip.exe',
+            ['*'] = 'clip.exe',
+        },
+        paste = {
+            ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+            ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+        },
+        cache_enabled = 0,
+    }
+end
 
